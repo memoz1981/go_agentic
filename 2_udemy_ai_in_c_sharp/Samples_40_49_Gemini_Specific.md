4df4b3a585f9f969c1cddb8e332cb09cc3177a2b
M.Z <zeynalov@gmail.com>
2026-06-28 14:27:28 +0400

    Sample41 - List Gemini Base Models

--- 2_udemy_ai_in_c_sharp/Samples_40_49_Gemini_Specific.md
+++ 2_udemy_ai_in_c_sharp/Samples_40_49_Gemini_Specific.md
@@ -3,3 +3,7 @@
 ### Sample 40 - First Gemini Demo
 - Just to see if API Key / setups are working
 - Almost same as Sample 1 - but this time using Gemini model
+
+### Sample 41 - List Gemini Base Models
+- Sample to list GEMINI Base models (can be retrieved from the client)
+- Wonder if OpenAI/Anthropic has this feature... but really useful one. 
---
+++ 2_udemy_ai_in_c_sharp/UdemyAICourseNotes/Samples/40_Gemini_Topics/41_List_Models.cs
@@ -0,0 +1,30 @@
+﻿using Google.GenAI;
+using UdemyAICourseNotes.Helpers;
+using static UdemyAICourseNotes.Helpers.Output;
+
+namespace UdemyAICourseNotes.Samples._40_Gemini_Topics
+{
+    internal class _41_List_Models : BaseSample
+    {
+        public override string Description => "List GEMINI Base Models";
+
+        public override async Task RunAsync()
+        {
+            Gray($"Running the sample for {Description}");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            var client = new Client(apiKey: SecretsManager.GetApiKey(Enums.Clients.Gemini));
+
+            var models = await client.Models.ListAsync();
+
+            BlueLine("Below is the list of base models for Google Gemini"); 
+            await foreach (var model in models)
+            {
+                Yellow($"{model.Name} ({model.DisplayName}) - ");
+                Green(model.Description);
+                Console.WriteLine(); 
+            }
+        }
+    }
+}
